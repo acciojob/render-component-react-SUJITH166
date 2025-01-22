848fd5b9292dbcba9ab0a371abc596fe4a01cd0e
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-01-22 20:52:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main"></div>
+  <div id="root">
+  <p>I am learning React. My life is getting better.
+  </p>
+  </div>
   )
 }
 
